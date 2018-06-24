2c6bd85ad9225d95f54db54281d7c190570e4361
Jane Liang <jane.w.liang@gmail.com>
2018-06-24 00:24:34 -0400

    Clean up, deleting extraneous things

--- woodruff_sim.R
+++ woodruff_sim.R
@@ -1,109 +1,140 @@
-library(MESS) # For AUC
-library(mutoss) # For adaptive BH
+library(MESS) # AUC
+library(mutoss) # adaptive Benjamini-Hochberg
 
 # 100 chemicals
 nchem = 100
 # 10 tissues
 ntiss = 10
 
-# Simulated X matrix of demographics
+# Read in simulated X matrix of demographics
 X = read.csv("./processed/woodruff_sim_X.csv", header=FALSE)
-# Simulated Y matrix 
+# Read in simulated Y matrix 
 Y = read.csv("./processed/woodruff_sim_Y.csv", header=FALSE)
-# Simulated interactions
-interactions = read.csv("./processed/woodruff_sim_interactions.csv", header=FALSE)
+# Read in simulated interactions
+interactions = read.csv("./processed/woodruff_sim_interactions.csv", 
+                        header=FALSE)
 
-# # Lambdas used for L1
-# lambdas = as.character(read.csv("./processed/woodruff_sim_lambdas.csv", header=FALSE)[,1])
-# # Some annoying manipulation to get lambdas to match file names
-# lambdas[lambdas=="1"] = "1.0"
-# lambdas[lambdas=="5e-04"] = "0.0005"
-# lambdas[lambdas=="1e-04"] = "0.0001"
-# lambdas[lambdas=="8e-05"] = "8.0e-5"
-# lambdas[lambdas=="6e-05"] = "6.0e-5"
-
-# Read in coefficient estimates from L1
-coeffs = read.csv("./processed/woodruff_sim_l1_coeffs.csv", header=FALSE)
-coeffs_list = lapply(1:length(coeffs), function(j){
-  matrix(coeffs[,j], dim(interactions)[1]+1, dim(interactions)[2]+1)
-})
-
-# TPR and FPR for L1
-tpr_l1 = sapply(1:length(coeffs_list), function(i){
-  sum((coeffs_list[[i]][-1, -1][,-(1:(nchem+ntiss))] == 0) & (interactions[,-(1:(nchem+ntiss))] == 0)) /sum(interactions[,-(1:(nchem+ntiss))] == 0)
-})
-fpr_l1 = 1 - sapply(1:length(coeffs_list), function(i){
-  sum((coeffs_list[[i]][-1, -1][,-(1:(nchem+ntiss))] != 0) & (interactions[,-(1:(nchem+ntiss))] != 0)) /sum(interactions[,-(1:(nchem+ntiss))] != 0)
+# Read in L1 coefficient estimates
+woodruff_sim_l1_coeffs = read.csv("./processed/woodruff_sim_l1_coeffs.csv", 
+                                  header=FALSE)
+# Reshape the coefficients so that there is a separate matrix for each lambda
+coeffs_list = lapply(1:length(woodruff_sim_l1_coeffs), function(j){
+  matrix(woodruff_sim_l1_coeffs[,j], 
+         dim(interactions)[1]+1, dim(interactions)[2]+1)
 })
 
 
-# Run linear regression one by one for each chemical x tissue and get p-values
-pvals_linreg = sapply(1:ncol(Y), function(j){
+# Run linear regression one-by-one for each chemical x tissue combination 
+# and get p-values
+p_lm = sapply(1:ncol(Y), function(j){
   summary(lm(y~., data=data.frame(y=Y[,j], X)))$coefficients[-1,4]
 })
-# B-H adjusted p-values
-pvalsa_linreg = matrix(adaptiveBH(as.matrix(pvals_linreg), alpha=0.05, silent=TRUE)$adjPValues, ncol=ncol(pvals_linreg))
-
-# Cutoffs
-FDR = c(10^(seq(-164, -44, by=20)), 10^(seq(-40, -4, by=4)), seq(0.1,1,length=20))
-
-# TPR and FPR from univariate linear regression
-tprall = sapply(1:length(FDR), function(i){
-  sum((pvalsa_linreg <= FDR[i]) & (interactions[,-(1:(nchem+ntiss))] != 0)) /sum(interactions[,-(1:(nchem+ntiss))] != 0)
-})
-fprall = sapply(1:length(FDR), function(i){
-  sum((pvalsa_linreg <= FDR[i]) & (interactions[,-(1:(nchem+ntiss))] == 0)) /sum(interactions[,-(1:(nchem+ntiss))] == 0)
-})
+# Apply adaptive Benjamini-Hochberg adjustment to p-values
+p.adj_lm = matrix(adaptiveBH(as.matrix(p_lm), alpha=0.05, 
+                             silent=TRUE)$adjPValues, ncol=ncol(p_lm))
 
 
-# Map interaction space to chemicals
-B_reduce_chem = function (B, nchem, ntiss, fun=mean) {
-  B_reduced = matrix(nrow=nrow(B), ncol=nchem)
+# Positives
+P_mat = interactions[,-(1:(nchem+ntiss))] != 0
+# Negatives
+N_mat = interactions[,-(1:(nchem+ntiss))] == 0
+# FDR cutoffs, chosen to get smooth curves
+FDR = c(10^(seq(-164, -44, by=20)), 10^(seq(-40, -4, by=4)), 
+        seq(0.1,1,length=20))
+
+
+# Function to map interaction space to chemicals
+# interact = interactions
+# nchem = number of chemicals
+# ntiss = number of tissues
+# FUN = function to apply for reduction
+inter2chem = function (interact, nchem, ntiss, FUN=mean) {
+  # Initialize output matrix
+  chem = matrix(nrow=nrow(interact), ncol=nchem)
+  # Reduce interactions by chemical
   for (j in 1:nchem) {
-    B_reduced[,j] = apply(B[,seq(j, by=nchem, length=ntiss)], 1, fun) 
+    chem[,j] = apply(interact[,seq(j, by=nchem, length=ntiss)], 1, FUN) 
   }
-  return(B_reduced)
+  return(chem)
 }
 
 # Repeated matrix of simulated chemical effects to map to chemicals
-inter_stack_chem = repmat(as.matrix(interactions[,1:nchem]), ncol=ntiss) 
+inter_chem_stack = repmat(as.matrix(interactions[,1:nchem]), ncol=ntiss) 
 
-# TPR and FPR when using number of hits to detect chemical significance
-tpr_chem = sapply(1:4, function(k){
+
+# TPR for L1 coefficient estimates
+tpr_mlm_l1 = sapply(1:length(coeffs_list), function(i){
+  sum((coeffs_list[[i]][-1, -1][,-(1:(nchem+ntiss))] != 0) & P_mat)
+}) / sum(P_mat)
+# FPR for L1 coefficient estimates
+fpr_mlm_l1 = sapply(1:length(coeffs_list), function(i){
+  sum((coeffs_list[[i]][-1, -1][,-(1:(nchem+ntiss))] != 0) & N_mat)
+}) / sum(N_mat)
+
+
+# TPR for univariate linear regression
+tpr_lm = sapply(1:length(FDR), function(i){
+  sum((p.adj_lm <= FDR[i]) & P_mat)
+}) / sum(P_mat)
+# FPR for univariate linear regression
+fpr_lm = sapply(1:length(FDR), function(i){
+  sum((p.adj_lm <= FDR[i]) & N_mat)
+}) / sum(N_mat)
+
+
+# TPR for univariate linear regression when using number of hits to detect 
+# chemical significance
+tpr_lm_chem = sapply(1:4, function(k){
   sapply(1:length(FDR), function(i){
-    sum(B_reduce_chem(((pvalsa_linreg <= FDR[i]) & (inter_stack_chem != 0)), nchem, ntiss) > k/5) /sum(interactions[,1:nchem] != 0)
+    sum(inter2chem(((p.adj_lm <= FDR[i]) & 
+                      (inter_chem_stack != 0)), nchem, ntiss) > k/5)
   })
-})
-fpr_chem = sapply(1:4, function(k){
-  sapply(1:length(FDR), function(i){
-    sum(B_reduce_chem(((pvalsa_linreg <= FDR[i]) & (inter_stack_chem == 0)), nchem, ntiss) > 1/5) /sum(interactions[,1:nchem] == 0)
-  })
-})
-sapply(1:4, function(j){auc(c(0,fpr_chem[,j],1), c(0,tpr_chem[,j],1))})
+}) / sum(interactions[,1:nchem] != 0)
+# FPR for univariate linear regression when using number of hits to detect 
+# chemical significance
+fpr_lm_chem = sapply(1:length(FDR), function(i){
+  sum(inter2chem(((p.adj_lm <= FDR[i]) & 
+                    (inter_chem_stack == 0)), nchem, ntiss) > 1/5)
+}) / sum(interactions[,1:nchem] == 0)
 
 
+# Calculate AUC for each method
+AUCs = c(MESS::auc(c(1,fpr_mlm_l1,0), c(1,tpr_mlm_l1,0), type="spline"), 
+         MESS::auc(c(0,fpr_lm,1), c(0,tpr_lm,1)),
+         sapply(1:4, function(j){
+           auc(c(0,fpr_lm_chem,1), c(0,tpr_lm_chem[,j],1))}))
 
-auc_chem = c(MESS::auc(c(1,fpr_l1,0), c(1,tpr_l1,0), type="spline"),
-             MESS::auc(c(0,fprall,1), c(0,tprall,1)),
-             sapply(1:4, function(j){auc(c(0,fpr_chem[,j],1), c(0,tpr_chem[,j],1))}))
+
 png("./pictures/woodruff_sim_ROC_chem.png", width=380, height=380)
 par(mar=c(4.1,4.1,1.1,1.1))
-plot(c(fpr_l1,0), c(tpr_l1,0), xlab="False Positive Rate", ylab="True Positive Rate", 
-     xaxs="i", yaxs="i", type="l") # L1
-lines(fprall, tprall, col="firebrick3") # Univariate linear regression for interactions
 
-# At least 1, 2, 3, and 4 hits
-lines(fpr_chem[,1], tpr_chem[,1], col="dodgerblue3")
-lines(fpr_chem[,2], tpr_chem[,2], col="dodgerblue3", lty="dashed")
-lines(fpr_chem[,3], tpr_chem[,3], col="dodgerblue3", lty="dotdash")
-lines(fpr_chem[,4], tpr_chem[,4], col="dodgerblue3", lty="dotted")
+# L1-penalized matrix linear models
+plot(c(fpr_mlm_l1,0), c(tpr_mlm_l1,0), 
+     xlab="False Positive Rate", ylab="True Positive Rate", 
+     xaxs="i", yaxs="i", type="l") 
 
-abline(0,1, col="grey") # Reference
+# Univariate linear regression for interactions
+lines(fpr_lm, tpr_lm, col="firebrick3") 
 
-leg_chem = paste0(c("Interactions",  "1/5 Hits", "2/5 Hits", "3/5 Hits", "4/5 Hits"), 
-                  " (", round(auc_chem[-1], 3), ")")
+# Univariate linear regression with at least 1/5 chemical hits
+lines(fpr_lm_chem, tpr_lm_chem[,1], col="dodgerblue3")
+# Univariate linear regression with at least 2/5 chemical hits
+lines(fpr_lm_chem, tpr_lm_chem[,2], col="dodgerblue3", lty="dashed")
+# Univariate linear regression with at least 3/5 chemical hits
+lines(fpr_lm_chem, tpr_lm_chem[,3], col="dodgerblue3", lty="dotdash")
+# Univariate linear regression with at least 4/5 chemical hits
+lines(fpr_lm_chem, tpr_lm_chem[,4], col="dodgerblue3", lty="dotted")
+
+# Reference line
+abline(0,1, col="grey") 
+
+# Legend for different methods
+leg_chem = paste0(c("Interactions",  "1/5 Hits", "2/5 Hits", 
+                    "3/5 Hits", "4/5 Hits"), 
+                  " (", round(AUCs[-1], 3), ")")
 legend(0.4, 0.4, 
-       c(expression(L[1]-penalized ~ (0.905)), leg_chem), 
+       c(bquote(L[1]-penalized~(.(round(AUCs[1], 3)))), 
+         sapply(leg_chem, function(x){bquote(.(x))})), 
        col=c("black", "firebrick3", rep("dodgerblue3", 4)), 
        lty=c(rep("solid",3), "dashed", "dotdash", "dotted"), bty="n")
 dev.off()
