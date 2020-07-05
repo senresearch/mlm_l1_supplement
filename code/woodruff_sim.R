2097b60f4b0d7d7aef1cae7056dc07568ea8a06c
Jane Liang <jane.w.liang@gmail.com>
2020-07-05 13:13:03 -0400

    Use different line types so distinguishing lines is less reliant on color

--- code/woodruff_sim.R
+++ code/woodruff_sim.R
@@ -100,9 +100,9 @@ lmChemFPR = sapply(1:length(FDR), function(i){
 
 # Calculate AUC for each method
 AUCs = c(MESS::auc(c(1,mlmL1FPR,0), c(1,mlmL1TPR,0), type="spline"), 
-         MESS::auc(c(0,lmFPR,1), c(0,lmTPR,1)),
+         MESS::auc(c(0,lmFPR,1), c(0,lmTPR,1), type="spline"),
          sapply(1:4, function(j){
-           auc(c(0,lmChemFPR,1), c(0,lmChemTPR[,j],1))}))
+           auc(c(0,lmChemFPR,1), c(0,lmChemTPR[,j],1), type="spline")}))
 
 
 png("../pictures/woodruff_sim_ROC_chem.png", width=380, height=380)
@@ -114,12 +114,12 @@ plot(c(mlmL1FPR,0), c(mlmL1TPR,0),
      xaxs="i", yaxs="i", type="l") 
 
 # Univariate linear regression for interactions
-lines(lmFPR, lmTPR, col="firebrick3") 
+lines(lmFPR, lmTPR, col="firebrick3", lty="dashed") 
 
 # Univariate linear regression with at least 1/5 chemical hits
-lines(lmChemFPR, lmChemTPR[,1], col="dodgerblue3")
+lines(lmChemFPR, lmChemTPR[,1], col="dodgerblue3", lty="longdash")
 # Univariate linear regression with at least 2/5 chemical hits
-lines(lmChemFPR, lmChemTPR[,2], col="dodgerblue3", lty="dashed")
+lines(lmChemFPR, lmChemTPR[,2], col="dodgerblue3", lty="twodash")
 # Univariate linear regression with at least 3/5 chemical hits
 lines(lmChemFPR, lmChemTPR[,3], col="dodgerblue3", lty="dotdash")
 # Univariate linear regression with at least 4/5 chemical hits
@@ -129,12 +129,13 @@ lines(lmChemFPR, lmChemTPR[,4], col="dodgerblue3", lty="dotted")
 abline(0,1, col="grey") 
 
 # Legend for different methods
-leg_chem = paste0(c("Interactions",  "1/5 Hits", "2/5 Hits", 
+legChem = paste0(c("Interactions",  "1/5 Hits", "2/5 Hits", 
                     "3/5 Hits", "4/5 Hits"), 
                   " (", round(AUCs[-1], 3), ")")
 legend(0.4, 0.4, 
        c(as.expression(bquote(L[1]-penalized ~ (.(round(AUCs[1], 3))))), 
-         sapply(leg_chem, function(x){bquote(.(x))})), 
+         sapply(legChem, function(x){bquote(.(x))})), 
        col=c("black", "firebrick3", rep("dodgerblue3", 4)), 
-       lty=c(rep("solid",3), "dashed", "dotdash", "dotted"), bty="n")
+       lty=c("solid", "dashed", "longdash", 
+             "twodash", "dotdash", "dotted"), bty="n")
 dev.off()
