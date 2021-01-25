b6f528ad02f8013ea1b68cbd09b46bc37596999e
Jane Liang <jane.w.liang@gmail.com>
2021-01-25 15:39:02 -0500

    Plot Agren main and interection effects on opposite sides of x-axis

--- code/agren.R
+++ code/agren.R
@@ -8,12 +8,13 @@ coeffs = matrix(agrenL1Coeffs[,lambda], 700, 2)
 png("../pictures/agren_l1_coeffs_lambda_6.2.png", width=380, height=150)
 par(mar=c(1.1,4.1,1.1,1.1))
 
-# Site interactions
-plot(1:700, abs(coeffs[,2]), type="l", 
-     xlab="", ylab="QTL Effects", xaxt="n", ylim=c(0,0.16))
-
 # Main effects
-lines(1:700, abs(coeffs[,1]), lty=2)
+plot(1:700, abs(coeffs[,1]), type="l", 
+     xlab="", ylab="QTL Effects", xaxt="n", 
+     ylim=c(min(-abs(coeffs[,2])), max(abs(coeffs[,1]))))
+
+# Site interactions
+lines(1:700, -abs(coeffs[,2]))
 
 # Reference lines to delineate chromosomes
 invisible(sapply(cumsum(c(173, 118, 137, 113)), 
