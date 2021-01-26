5d042aed078d8f6c2e4b709e44a66fea87423a42
Jane Liang <jane.w.liang@gmail.com>
2021-01-25 21:58:20 -0500

    Update Agren plot

--- code/agren.R
+++ code/agren.R
@@ -10,8 +10,9 @@ par(mar=c(1.1,4.1,1.1,1.1))
 
 # Main effects
 plot(1:700, abs(coeffs[,1]), type="l", 
-     xlab="", ylab="QTL Effects", xaxt="n", 
-     ylim=c(min(-abs(coeffs[,2])), max(abs(coeffs[,1]))))
+     xlab="", xaxt="n", yaxt="n", 
+     ylab="Absolute QTL Effects", 
+     ylim=max(abs(coeffs))*c(-1,1))
 
 # Site interactions
 lines(1:700, -abs(coeffs[,2]))
@@ -19,4 +20,12 @@ lines(1:700, -abs(coeffs[,2]))
 # Reference lines to delineate chromosomes
 invisible(sapply(cumsum(c(173, 118, 137, 113)), 
                  function(v){abline(v=v, col="grey", lty=3)}))
+
+# Customized y-axis without negative labels
+ticks = seq(-0.15, 0.15, by=0.05)
+axis(2, at = ticks, labels = abs(ticks))
+
+# Text labels for main and interaction effects
+text(700, 0.13, "Main effects", adj=1)
+text(700, -0.13, "Interactions", adj=1)
 dev.off()
